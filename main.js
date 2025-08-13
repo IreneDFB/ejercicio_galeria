098d451b51a0fca787f2c47da1c5c3e91b6f76f8
IreneDFB <irene_dfb@hotmail.com>
2025-08-13 19:31:21 +0200

    corrijo una falta de ortografía

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ const products = [
       'https://images.unsplash.com/photo-1679394042175-717ca34ef0f2?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
   {
-    name: 'SPECIAL PACK: MOURNING ROUTINE',
+    name: 'SPECIAL PACK: MORNING ROUTINE',
     price: 32.0,
     stars: 4.9,
     reviews: 1890,
